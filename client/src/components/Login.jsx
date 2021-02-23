9a411cee0166c9267cc39cd3c9734129f5d9fbdb
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2021-02-23 12:55:33 -0600

    1/2 of basic auth setup

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Login = () => {
+    return (
+        <>
+            <h1>Hello world! this is the Login page</h1>
+        </>
+    )
+}
+
+export default Login
