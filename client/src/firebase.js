0b217984118ff0e6a74954e8cbf42b10a631dd1d
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2021-02-24 09:11:32 -0600

    basic auth fully functioning

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,7 +3,7 @@ import "firebase/auth"
 
 const app = firebase.initializeApp({
    
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN, 
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
