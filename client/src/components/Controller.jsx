b58b41e7a86907705df8f23c5fc889f1122df0f9
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2021-03-10 10:47:51 -0600

    started controller component

---
+++ client/src/components/Controller.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Controller = () => {
+    return (
+        <>
+            <h1>This will be the controller component</h1>
+        </>
+    )
+}
+
+export default Controller
