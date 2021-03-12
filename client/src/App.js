381bec33900f6276c190a36bdd48ae5153ed5c8f
katiegerhart <katiegerhart@gmail.com>
2021-03-12 14:55:19 -0800

    Homepage with nav and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import React from "react"
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Dashboard from "./components/Dashboard"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
+import ProfessionalsForm from "./components/ProfessionalsForm"
 import { AuthProvider } from "./Auth.js"
 import PrivateRoute from "./PrivateRoute"
+import MyNavbar from './components/Navbar'
+import MyFooter from './components/MyFooter'; 
 
 function App() {
   return (
     <AuthProvider>
       <Route>
         <div>
+          <MyNavbar />
           <PrivateRoute exact path="/" component={Dashboard} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
+          <Route exact path="/professionalsform" component={ProfessionalsForm} />
+          <MyFooter />
         </div>
       </Route>
     </AuthProvider>
